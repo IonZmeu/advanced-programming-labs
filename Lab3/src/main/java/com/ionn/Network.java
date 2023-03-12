2a9c06887a73fffaa565a1574a02c73c8df4b13f
DESKTOP-KJNB9D5\Ion <zmeuion666666@gmail.com>
2023-03-12 19:34:16 +0200

    done compulsory + homework

--- Lab3/src/main/java/com/ionn/Network.java
+++ Lab3/src/main/java/com/ionn/Network.java
@@ -1,10 +1,7 @@
 package com.ionn;
 
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Network {
     private List<Person> personList = new ArrayList<>();
